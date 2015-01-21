6be1e7f665af6f0df20c5cc879c51911ab17b420
afinebojangle <rayford.taylor@gmail.com>
2015-01-21 22:18:49 +0000

    fixed vote spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,8 +3,17 @@ require 'rails_helper'
 describe Vote do
   describe "validations" do
     describe "value validation" do
+      
+      before do
+        v = Vote.new(value: 1)
+        v2 = Vote.new(value: -1)
+        v3 = Vote.new(value: 2)
+      end
+      
       it "only allows 1 or -1 as values" do
-        expect( vote.value ).to eq(1 || -1)
+        expect( v.valid? ).to eq(true)
+        expect( v2.valid? ).to eq(true)
+        expect( v3.valid? ).to eq(false)
       end
     end
   end
